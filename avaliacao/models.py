1dc9a472b1f3800bf0da5e0ac4d27b74b73bf771
Everton Plasse Volkweis <evertonvolkweis@outlook.com>
2023-03-08 23:30:58 +0000

    Development 11

--- avaliacao/models.py
+++ avaliacao/models.py
@@ -13,3 +13,24 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(140))
     password = db.Column(db.String(512))
+
+
+class NotaAvalia(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    valor = db.Column(db.Numeric)
+    comentario = db.Column(db.String(255))
+    fk_avaliacao_id = db.Column(db.Integer, db.ForeignKey('avaliacao.id'))
+    fk_habilidade_atitude_id = db.Column(db.Integer, db.ForeignKey('habilidade_atitude.id'))
+
+class Avaliacao(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(60))
+    descricao = db.Column(db.String(255))
+    notas = db.relationship('NotaAvalia', backref='avaliacao')
+
+class HabilidadeAtitude(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(60))
+    fator_peso = db.Column(db.Numeric)
+    descricao = db.Column(db.String(255))
+    notas = db.relationship('NotaAvalia', backref='habilidade_atitude')
