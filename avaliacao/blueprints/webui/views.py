65aee5b5a11147f9485c1b60922eca8d4545de33
Lincon Nascimento <linconrobertonascimento@gmail.com>
2023-03-12 18:10:02 +0000

    Development 17

--- avaliacao/blueprints/webui/views.py
+++ avaliacao/blueprints/webui/views.py
@@ -1,6 +1,6 @@
 from flask import abort, render_template, request
 from avaliacao.ext.main import MATRIZ_AVALIACAO, calcular_media
-from avaliacao.models import Avaliacao, HabilidadeAtitude, NotaAvalia
+from avaliacao.models import Avaliacao, HabilidadeAtitude, NotaAvalia, Usuario
 from avaliacao.ext.database import db
 
 
@@ -12,7 +12,8 @@ def inserir_notas():
     return render_template("inserir.html", matriz_avaliacao = matriz)
 
 def cadastro_avaliacao():
-    return render_template("cadastro-avaliacao.html")
+    usuario = Usuario.query.all()
+    return render_template("cadastro-avaliacao.html", usuarios=usuario)
 
 
 
